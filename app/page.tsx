3775ed0abda36939add972ff917e4137bc664f2a
Moyomade <adegbitemoyomade2004@gmail.com>
2024-01-12 00:09:33 +0100

    Updated page.tsx file

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 
-
 export default function Home() {
 	return (
 		<div>
-			
-			<h2>Subscribe to my channel</h2>
+			<div className="grid grid-cols-1 md:grid-cols-3 bg-blue-100">
+				<div>Booking</div>
+				<div className="col-span-2 bg-red-100 order-first md:order-last">
+					Map
+				</div>
+			</div>
 		</div>
 	);
 }
