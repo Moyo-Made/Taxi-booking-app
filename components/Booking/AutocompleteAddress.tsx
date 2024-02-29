53b51d07ceea8d775ddaff585ebfbb8604e8f4a8
Moyomade <adegbitemoyomade2004@gmail.com>
2024-02-29 18:28:31 +0100

    Added auto complete for address

--- components/Booking/AutocompleteAddress.tsx
+++ components/Booking/AutocompleteAddress.tsx
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 function AutocompleteAddress() {
 	const [source, setSource] = useState<any>();
-	const getAddress = async () => {
-		const res = fetch("api/search-address?q=" + source, {
+	const [addressList, setAddressList] = useState<any>([]);
+
+	useEffect(() => {
+		const delayDebounceFn = setTimeout(() => {}, 1000);
+		return () => clearTimeout(delayDebounceFn);
+	}, [source]);
+	const getAddressList = async () => {
+		const res = await fetch("api/search-address?q=" + source, {
 			headers: {
 				"Content-Type": "application/json",
 			},
 		});
-
 		const result = await res.json();
+		setAddressList(result);
 	};
 	return (
 		<div className="mt-5">
@@ -19,7 +26,16 @@ function AutocompleteAddress() {
 					type="text"
 					className="bg-white p-1 border-[1px] w-full rounded-md outline-none
                     focus:border-yellow-300"
+					value={source}
+					onChange={(e) => setSource(e.target.value)}
 				/>
+				{addressList?.suggestions ? (
+					<div>
+						{addressList?.suggestions.map((item: any, index: number) => (
+							<h2>{item.full_address}</h2>
+						))}
+					</div>
+				) : null}
 			</div>
 
 			<div className="mt-3">
